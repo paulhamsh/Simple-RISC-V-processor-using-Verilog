944dafaffe5a5b04526959711e021fedc9100bb2
Paul Hamshere <p.w.hamshere@gmail.com>
2026-03-06 13:45:15 +0000

    Add files via upload

---
+++ Vivado/scripts/L-Build.tcl
@@ -0,0 +1,31 @@
+set PROJECT L-Data-Misaligned
+set DIR $PATH/$PROJECT
+
+create_project -force $PROJECT $DIR -part $PART
+
+add_files $DIR/src/alu.v 
+add_files $DIR/src/branch.v 
+add_files $DIR/src/control_unit.v 
+add_files $DIR/src/data_memory.v
+add_files $DIR/src/imm.v 
+add_files $DIR/src/instruction_memory.v 
+add_files $DIR/src/processor.v 
+add_files $DIR/src/registers.v 
+
+add_files $DIR/src/test_prog.mem 
+add_files $DIR/src/test_data_ramA.mem
+add_files $DIR/src/test_data_ramB.mem
+add_files $DIR/src/test_data_ramC.mem 
+add_files $DIR/src/test_data_ramD.mem 
+
+add_files -fileset sim_1 $DIR/src/testbench_data_memory_misaligned.v 
+add_files -fileset sim_1 $DIR/src/testbench_data_memory_misaligned_integrated.v 
+add_files -fileset sim_1 $DIR/src/testbench_data_memory_misaligned_integrated_behav.wcfg 
+
+import_files -force
+
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
+close_project
+
