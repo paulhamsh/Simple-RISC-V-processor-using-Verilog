dabdffc15f954f4c8ad23ba0dd2cd2f0e0564e9e
Paul Hamshere <p.w.hamshere@gmail.com>
2026-03-06 13:55:34 +0000

    Add files via upload

---
+++ Quartus/scripts/create.bat
@@ -0,0 +1,72 @@
+mkdir ..\A-Program-Counter\src
+mkdir ..\B-Instruction-Memory\src
+mkdir ..\C-Control-Unit\src
+mkdir ..\D-Registers\src
+mkdir ..\E-ALU\src
+mkdir ..\F-Immediates\src
+mkdir ..\G-Branch\src
+mkdir ..\H-Jumps\src
+mkdir ..\I-Large-Immediate\src
+mkdir ..\J-Data-Word\src
+mkdir ..\K-Data-Aligned\src
+mkdir ..\L-Data-Misaligned\src
+mkdir ..\M-Input-Output\src
+mkdir ..\N-SOC\src
+mkdir ..\A-Program-Counter\working\simulation\modelsim
+mkdir ..\B-Instruction-Memory\working\simulation\modelsim
+mkdir ..\C-Control-Unit\working\simulation\modelsim
+mkdir ..\D-Registers\working\simulation\modelsim
+mkdir ..\E-ALU\working\simulation\modelsim
+mkdir ..\F-Immediates\working\simulation\modelsim
+mkdir ..\G-Branch\working\simulation\modelsim
+mkdir ..\H-Jumps\working\simulation\modelsim
+mkdir ..\I-Large-Immediate\working\simulation\modelsim
+mkdir ..\J-Data-Word\working\simulation\modelsim
+mkdir ..\K-Data-Aligned\working\simulation\modelsim
+mkdir ..\L-Data-Misaligned\working\simulation\modelsim
+mkdir ..\M-Input-Output\working\simulation\modelsim
+mkdir ..\N-SOC\working\simulation\modelsim
+copy /y ..\..\A-Program-Counter\src ..\A-Program-Counter\src
+copy /y ..\..\B-Instruction-Memory\src ..\B-Instruction-Memory\src
+copy /y ..\..\C-Control-Unit\src ..\C-Control-Unit\src
+copy /y ..\..\D-Registers\src ..\D-Registers\src
+copy /y ..\..\E-ALU\src ..\E-ALU\src
+copy /y ..\..\F-Immediates\src ..\F-Immediates\src
+copy /y ..\..\G-Branch\src ..\G-Branch\src
+copy /y ..\..\H-Jumps\src ..\H-Jumps\src
+copy /y ..\..\I-Large-Immediate\src ..\I-Large-Immediate\src
+copy /y ..\..\J-Data-Word\src ..\J-Data-Word\src
+copy /y ..\..\K-Data-Aligned\src ..\K-Data-Aligned\src
+copy /y ..\..\L-Data-Misaligned\src ..\L-Data-Misaligned\src
+copy /y ..\..\M-Input-Output\src ..\M-Input-Output\src
+copy /y ..\..\N-SOC\src ..\N-SOC\src
+copy /y ..\..\B-Instruction-Memory\src\*.mem ..\B-Instruction-Memory\working\simulation\modelsim
+copy /y ..\..\C-Control-Unit\src\*.mem ..\C-Control-Unit\working\simulation\modelsim
+copy /y ..\..\D-Registers\src\*.mem ..\D-Registers\working\simulation\modelsim
+copy /y ..\..\E-ALU\src\*.mem ..\E-ALU\working\simulation\modelsim
+copy /y ..\..\F-Immediates\src\*.mem ..\F-Immediates\working\simulation\modelsim
+copy /y ..\..\G-Branch\src\*.mem ..\G-Branch\working\simulation\modelsim
+copy /y ..\..\H-Jumps\src\*.mem ..\H-Jumps\working\simulation\modelsim
+copy /y ..\..\I-Large-Immediate\src\*.mem ..\I-Large-Immediate\working\simulation\modelsim
+copy /y ..\..\J-Data-Word\src\*.mem ..\J-Data-Word\working\simulation\modelsim
+copy /y ..\..\K-Data-Aligned\src\*.mem ..\K-Data-Aligned\working\simulation\modelsim
+copy /y ..\..\L-Data-Misaligned\src\*.mem ..\L-Data-Misaligned\working\simulation\modelsim
+copy /y ..\..\M-Input-Output\src\*.mem ..\M-Input-Output\working\simulation\modelsim
+copy /y ..\..\N-SOC\src\*.mem ..\N-SOC\working\simulation\modelsim
+copy /y A-Program-Counter.q* ..\A-Program-Counter\working
+copy /y B-Instruction-Memory.q* ..\B-Instruction-Memory\working
+copy /y C-Control-Unit.q* ..\C-Control-Unit\working
+copy /y D-Registers.q* ..\D-Registers\working
+copy /y E-ALU.q* ..\E-ALU\working
+copy /y F-Immediates.q* ..\F-Immediates\working
+copy /y G-Branch.q* ..\G-Branch\working
+copy /y H-Jumps.q* ..\H-Jumps\working
+copy /y I-Large-Immediate.q* ..\I-Large-Immediate\working
+copy /y J-Data-Word.q* ..\J-Data-Word\working
+copy /y K-Data-Aligned.q* ..\K-Data-Aligned\working
+copy /y L-Data-Misaligned.q* ..\L-Data-Misaligned\working
+copy /y M-Input-Output.q* ..\M-Input-Output\working
+copy /y N-SOC.q* ..\N-SOC\working
+
+
+
