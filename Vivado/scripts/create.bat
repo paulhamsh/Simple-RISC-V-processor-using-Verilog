1a0640d4287b70f686a72a1479d715f0c24c5802
Paul Hamshere <p.w.hamshere@gmail.com>
2026-03-06 15:29:05 +0000

    Update create.bat to copy source files
    Replaced robocopy command with multiple copy commands to copy source files from various directories.

--- Vivado/scripts/create.bat
+++ Vivado/scripts/create.bat
@@ -1,1 +1,14 @@
-robocopy ..\..\. .. /s /xd  Vivado Quartus
+copy /y ..\..\A-Program-Counter\src ..\A-Program-Counter\src
+copy /y ..\..\B-Instruction-Memory\src ..\B-Instruction-Memory\src
+copy /y ..\..\C-Control-Unit\src ..\C-Control-Unit\src
+copy /y ..\..\D-Registers\src ..\D-Registers\src
+copy /y ..\..\E-ALU\src ..\E-ALU\src
+copy /y ..\..\F-Immediates\src ..\F-Immediates\src
+copy /y ..\..\G-Branch\src ..\G-Branch\src
+copy /y ..\..\H-Jumps\src ..\H-Jumps\src
+copy /y ..\..\I-Large-Immediate\src ..\I-Large-Immediate\src
+copy /y ..\..\J-Data-Word\src ..\J-Data-Word\src
+copy /y ..\..\K-Data-Aligned\src ..\K-Data-Aligned\src
+copy /y ..\..\L-Data-Misaligned\src ..\L-Data-Misaligned\src
+copy /y ..\..\M-Input-Output\src ..\M-Input-Output\src
+copy /y ..\..\N-SOC\src ..\N-SOC\src
